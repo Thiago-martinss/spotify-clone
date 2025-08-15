541664a26801a71108499abed184dd76338dcae6
Thiago <martinsmthiago@gmail.com>
2025-08-15 20:21:21 -0300

    created isAdmin middleware

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -36,6 +36,16 @@ const protect = asyncHandler(async (req, res, next) => {
   }
 });
 
+const isAdmin = asyncHandler(async (req, res, next) => {
+  if (req.user && req.user.isAdmin) {
+    next();
+  } else {
+    res.status(StatusCodes.UNAUTHORIZED);
+    throw new Error('Not authorized as an admin');
+  }
+});
+
 module.exports = {
   protect,
+  isAdmin,
 };
--- src/routes/artistRoutes.js
+++ src/routes/artistRoutes.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const { protect } = require('../middlewares/auth');
+const { protect, isAdmin } = require('../middlewares/auth');
 const { createArtist } = require('../controllers/artistController');
 const upload = require('../middlewares/upload');
 
 const artistRouter = express.Router();
 
 //Public routes
-artistRouter.post("/", protect, upload.single("image"), createArtist);
+artistRouter.post('/', protect, isAdmin, upload.single('image'), createArtist);
 
 module.exports = artistRouter;
