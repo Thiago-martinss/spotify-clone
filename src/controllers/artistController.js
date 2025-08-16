1889673f177c66f72978d7feccb501fb0b51a68f
Thiago <martinsmthiago@gmail.com>
2025-08-15 21:10:00 -0300

    Update Artist and get Artist by ID

--- src/controllers/artistController.js
+++ src/controllers/artistController.js
@@ -1,9 +1,10 @@
-const asyncHandler = require("express-async-handler");
-const { StatusCodes } = require("http-status-codes");
-const Artist = require("../models/Artist");
-const Album = require("../models/Album");
-const Song = require("../models/Song");
-const { uploadToCloudinary } = require("../utils/cloudinaryUpload");
+const asyncHandler = require('express-async-handler');
+const { StatusCodes } = require('http-status-codes');
+const Artist = require('../models/Artist');
+const Album = require('../models/Album');
+const Song = require('../models/Song');
+const { uploadToCloudinary } = require('../utils/cloudinaryUpload');
+const { parse } = require('dotenv');
 
 //@desc -Create a new Artist
 //@route - POST /api/artists
@@ -13,24 +14,24 @@ const createArtist = asyncHandler(async (req, res) => {
   //Check if req.body is defined
   if (!req.body) {
     res.status(StatusCodes.BAD_REQUEST);
-    throw new Error("Request body is required");
+    throw new Error('Request body is required');
   }
   const { name, bio, genres } = req.body;
   //Validations
   if (!name || !bio || !genres) {
     res.status(StatusCodes.BAD_REQUEST);
-    throw new Error("Name, bio, genres are required");
+    throw new Error('Name, bio, genres are required');
   }
   //check if artist already exists
   const existingArtist = await Artist.findOne({ name });
   if (existingArtist) {
     res.status(StatusCodes.BAD_REQUEST);
-    throw new Error("Artist already exists");
+    throw new Error('Artist already exists');
   }
   //upload artist image if provided
-  let imageUrl = "";
+  let imageUrl = '';
   if (req.file) {
-    const result = await uploadToCloudinary(req.file.path, "spotify/artists");
+    const result = await uploadToCloudinary(req.file.path, 'spotify/artists');
     imageUrl = result.secure_url;
   }
 
@@ -45,4 +46,68 @@ const createArtist = asyncHandler(async (req, res) => {
   res.status(StatusCodes.CREATED).json(artist);
 });
 
-module.exports = { createArtist };
+const getArtists = asyncHandler(async (req, res) => {
+  const { genre, search, page = 1, limit = 10 } = req.query;
+  //Build filter object
+  const filter = {};
+  if (genre) filter.genres = { $in: [genre] };
+  if (search) {
+    filter.$or = [
+      { name: { $regex: search, $options: "i" } },
+      { bio: { $regex: search, $options: "i" } },
+    ];
+  }
+
+  //Count total artists with filter
+  const count = await Artist.countDocuments(filter);
+  //Pagination
+  const skip = (parseInt(page) - 1) * parseInt(limit);
+  //Get artists
+  const artists = await Artist.find(filter)
+    .sort({ followers: -1 })
+    .limit(parseInt(limit))
+    .skip(skip);
+  res.status(StatusCodes.OK).json({
+    artists,
+    page: parseInt(page),
+    pages: Math.ceil(count / parseInt(limit)),
+    totalArtists: count,
+  });
+});
+
+
+const getArtistsById = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  const artist = await Artist.findById(id);
+  if (!artist) {
+    res.status(StatusCodes.NOT_FOUND);
+    throw new Error("Artist not found");
+  }
+  res.status(StatusCodes.OK).json(artist);
+});
+
+
+const updateArtist = asyncHandler(async (req, res) => {
+  const { name, bio, genres, isVerified } = req.body;
+  const artist = await Artist.findById(req.params.id);
+  if (!artist) {
+    res.status(StatusCodes.NOT_FOUND);
+    throw new Error("Artist not found");
+  }
+  //Update artist details
+  artist.name = name || artist.name;
+  artist.bio = bio || artist.bio;
+  artist.genres = genres || artist.genres;
+  artist.isVerified =
+    isVerified !== undefined ? isVerified === "true" : artist.isVerified;
+  //Update image if provided
+  if (req.file) {
+    const result = await uploadToCloudinary(req.file.path, "spotify/artists");
+    artist.image = result.secure_url;
+  }
+  //reSave
+  const updatedArtist = await artist.save();
+  res.status(StatusCodes.OK).json(updatedArtist);
+});
+
+  module.exports = { createArtist, getArtists, getArtistsById, updateArtist };
--- src/routes/artistRoutes.js
+++ src/routes/artistRoutes.js
@@ -1,11 +1,29 @@
 const express = require('express');
 const { protect, isAdmin } = require('../middlewares/auth');
-const { createArtist } = require('../controllers/artistController');
+const {
+  getArtists,
+  getArtistsById,
+  updateArtist,
+  createArtist,
+} = require('../controllers/artistController');
 const upload = require('../middlewares/upload');
 
 const artistRouter = express.Router();
 
 //Public routes
+artistRouter.get('/', getArtists);
+artistRouter.get('/:id', getArtistsById);
+
+//Private routes
+
+//Admin
 artistRouter.post('/', protect, isAdmin, upload.single('image'), createArtist);
+artistRouter.put(
+  '/:id',
+  protect,
+  isAdmin,
+  upload.single('image'),
+  updateArtist
+);
 
 module.exports = artistRouter;
